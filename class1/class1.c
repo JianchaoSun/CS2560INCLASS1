cdf236485236c697d0950e8db9ad6661ae369cbf
jianchaosun <clarksun97@gmail.com>
2019-02-14 18:22:17 -0800

    inclass2

--- class1/class1.c
+++ class1/class1.c
@@ -6,52 +6,22 @@
  */
 #include <stdio.h>
 
+ 
 
+main()
 
-int main () {
+{
+
+   int a , b;
 
  
 
-   /* local variable definition */
+   a = 10;
 
-   int a = 100;
+   printf( "Value of b is %d\n", (a == 1) ? 20: 30 );
 
+ 
 
-
-   /* check the boolean condition */
-
-   if( a == 10 ) {
-
-      /* if condition is true then print the following */
-
-      printf("Value of a is 10\n" );
-
-   } else if( a == 20 ) {
-
-      /* if else if condition is true */
-
-      printf("Value of a is 20\n" );
-
-   } else if( a == 30 ) {
-
-      /* if else if condition is true  */
-
-      printf("Value of a is 30\n" );
-
-   } else {
-
-      /* if none of the conditions is true */
-
-      printf("None of the values is matching\n" );
-
-   }
-
-
-
-   printf("Exact value of a is: %d\n", a );
-
-
-
-   return 0;
+   printf( "Value of b is %d\n", (a == 10) ? 20: 30 );
 
 }
