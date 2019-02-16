f9af92b24d068f2140e422ba31ba653e098c18a2
jianchaosun <clarksun97@gmail.com>
2019-02-15 16:11:06 -0800

    inclass12

--- class1/class1.c
+++ class1/class1.c
@@ -5,6 +5,7 @@
  *      Author: jianchaosun
  */
 
+//another example
 #include <stdio.h>
 #include <string.h>
 
@@ -19,13 +20,14 @@ int main( ) {
    union Data data;
  
    data.i = 10;
-   data.f = 220.5;  //writes over i
-   strcpy( data.str, "C Programming");  //writes over f
- 
-   printf( "data.i : %d\n", data.i);    //print out garbage will read 4 bytes interpret as whole number
-   printf( "data.f : %f\n", data.f);    //print out garbage will read 4 bytes intrepret as
-   printf( "data.str : %s\n", data.str); //print out "C Programming"
+   printf( "data.i : %d\n", data.i);
+
+   data.f = 220.5;
+   printf( "data.f : %f\n", data.f);
+
+   strcpy( data.str, "C Programming");
+   printf( "data.str : %s\n", data.str);
  
    return 0;
 }
- 
+
